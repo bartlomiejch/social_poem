6dd91db7e2bf711c9a02119b3b9395de64deeae0
Bartlomiej Chowaniec <bartek.chowaniec@gmail.com>
2014-01-09 22:41:21 +0100

    Update forms.py

--- forms.py
+++ forms.py
@@ -2,4 +2,4 @@ from social_poem.models import Stropha
 from django import forms
 
 class StrophaAddForm(forms.Form):
-	added_strofa = forms.CharField(max_length=200)
+	add_stropha = forms.CharField(max_length=200)
