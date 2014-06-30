eff316211e3edf5c1661b4ad9f6e2430de3b920a
Bartlomiej Chowaniec <bartek.chowaniec@gmail.com>
2014-06-30 14:21:54 +0200

    Create views.py

---
+++ views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render_to_response
+from django.core.context_processors import csrf 
+
+from social_poem.models import Stropha
+from social_poem.forms import StrophaAddForm
+
+
+def poem(request):
+	if request.method == "POST":
+		form = StrophaAddForm(request.POST)
+		if form.is_valid():
+			my_model = Stropha()
+			my_model.stropha = form.cleaned_data['add_stropha']
+			my_model.save()
+
+	form = StrophaAddForm()
+	poetry = Stropha.objects.all()
+	c ={'poetry': poetry, 'form':form}
+	c.update(csrf(request))
+	return render_to_response('social_poem/social_poem.html', c)
